df7ea18c1b6119ad4666340180cb8709ddce5868
Matt Linaberry <mtlinaberry@gmail.com>
2018-01-06 12:23:00 -0500

    hooray done with exercise 112

--- firstNodeJSExercise/app.js
+++ firstNodeJSExercise/app.js
@@ -1,6 +1,27 @@
 var calc = require('./calculator');
+var readline = require('readline-sync');
 
-console.log('add function is ' + calc.add(5, 9));
-console.log("the subtract funciton is " + calc.subtract(58, 19));
-console.log("the multiply function is " + calc.multiply(8, 6));
-console.log("the divide function is " + calc.divide(14, 7));
+var firstNumber = readline.question("hey bud, can i have a number? ");
+var secondNumber = readline.question("hey bud, can i have another number? ");
+var operations = ['+', '-', '*', '/'];
+var theSelectedOp = readline.keyInSelect(operations, "hey bud, what would you like for me to do with these numbers?");
+var result;
+switch(theSelectedOp) {
+  case 0:
+    result = calc.add(firstNumber, secondNumber);
+    break;
+  case 1:
+    result = calc.subtract(firstNumber, secondNumber);
+    break;
+  case 2:
+    result = calc.multiply(firstNumber, secondNumber);
+    break;
+  case 3:
+    result = calc.divide(firstNumber, secondNumber);
+    break;
+  default:
+    result = -1
+    break;
+};
+
+console.log("The result of " + firstNumber + " " + operations[theSelectedOp] + " " + secondNumber + " = " + result);
